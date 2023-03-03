b730c52385b095806e0392e6a4e4d8140647ca6a
Kate Belson <kfb206@exeter.ac.uk>
2023-03-03 12:54:47 +0000

    first recursive commit

---
+++ factorial/factorial_test.go
@@ -0,0 +1,34 @@
+package factorial
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestUnitFactorial(t *testing.T) {
+	assert := assert.New(t)
+	tests :=
+		[]struct {
+			n      int
+			result int
+		}{
+			{0, 1},
+			{1, 1},
+			{3, 6},
+			{2, 2},
+			{4, 24},
+			{5, 120},
+			{6, 720},
+			{7, 5040},
+			{8, 40320},
+			{9, 362880},
+			{10, 3628800},
+		}
+
+	for _, test := range tests {
+		/* act */
+		v := Factorial(test.n)
+		/* assert */
+		assert.Equal(test.result, v)
+	}
+}
